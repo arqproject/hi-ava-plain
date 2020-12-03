8e20b0df6ac77b51928ac24206dac091110c2b96
arqproject <63571769+arqproject@users.noreply.github.com>
2020-12-03 23:19:41 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -8,7 +8,7 @@ const variables = {
   },
   quotes: [
     { quote: "Tepat hari ini, Hari dimana kamu dilahirkan, hari yang spesial untukmu dikala usiamu semakin menumbuh. Selamat Ulang Tahun Buat PuPuttt🥰🤗", author: "ariqoh" },
-    { quote: "Ariqq minta maaf yaa putt, ngga bisa ngasih kamu kado:*", author: "ariqoh" },
+    { quote: "Tepat hari ini, Hari dimana kamu dilahirkan, hari yang spesial untukmu dikala usiamu semakin menumbuh. Selamat Ulang Tahun Buat PuPuttt🥰🤗", author: "ariqoh" },
     { quote: "Puputt sehat selalu yaaa jaga diri baik²", author: "ariqoh" },
     { quote: "Dihari ulang tahunnya puputt kali ini, semoga puput bisaa jdi jodohku xixixi", author: "ariqoh" },
     { quote: "Love chuuu puputt🤗❤", author: "ariqoh" },
